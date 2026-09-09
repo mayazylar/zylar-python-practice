f4612a4052b7c1f561f5dc934ba03daa246e6488
zsonicdev <225995690+mayazylar@users.noreply.github.com>
2026-09-08 19:15:15 -0700

    Added case-insensitive answers, error handling, and a third question

---
+++ Day_3_Zylar.py
@@ -0,0 +1,5 @@
+my_info = {"name": "Zylar", "Age": 25, "Origin": "Burundi"}
+
+for key in my_info:
+    print(key + ": " + str(my_info[key]))
+    
--- hello_zylar.py
+++ hello_zylar.py
@@ -1,11 +1,23 @@
 score = 0
 
 answer1 = input("What's the capital of burundi ? ")
-if answer1 == "bujumbura":
+if answer1.lower() == "bujumbura":
     score = score + 1
 
 answer2 = input("What's the capital of switzerland? ")
-if answer2 == "bern":
+if answer2.lower() == "bern":
     score = score + 1
 
-print("you got "  + str(score) + " out of 2 correct")
+answer3 = input("How many continents do we have ?")
+try:
+    answer3 = int(answer3)
+    if answer3 == 7:
+        score = score + 1
+        print(f"Correct! There are {answer3} continents")
+    else:
+        print("incorrect answer")
+
+except:
+   print("That's not a valid number")
+   
+print(f"you got  {score}  out of 3 correct")
